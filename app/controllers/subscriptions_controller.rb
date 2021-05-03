58dfb9b68bdd2cd756933a78a948c3b7a06b22dc
Marji Symonds <marji712@gmail.com>
2021-05-03 09:25:37 -0700

    Update subscriptions controller and user model

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,11 @@ class SubscriptionsController < ApplicationController
     before_action :set_plan, only: [:new, :create, :update]
 
     def new
-        byebug
+    end
+
+    def create
+        current_user.update_card(params[:payment_method_id]) if params[:payment_method_id].present?
+        current_user.subscript(@plan.stripe_id)
     end
 
     private
