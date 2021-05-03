d46a0478217d0611697fcc586b761a57d705a94c
Marji Symonds <marji712@gmail.com>
2021-05-03 09:52:10 -0700

    Remake tables with stripe_price_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Movie.create(title: "The Matrix", video_url: "secret")
 
-Plan.create(name: "Small", stripe_id: "small-monthly", stipe_price_id: "price_1IjUjlBsEkZw4Q05BaQsnkY4", amount: 25_00, interval: "month")
-Plan.create(name: "Small", stripe_id: "small-annual", stipe_price_id: "price_1IjUjUBsEkZw4Q05RXEzOI1s", amount: 75_00, interval: "year")
-Plan.create(name: "Large", stripe_id: "large-monthly", stipe_price_id: "price_1IjUi4BsEkZw4Q05a7b6Q0Kc", amount: 100_00, interval: "month")
-Plan.create(name: "Large", stripe_id: "large-annual", stipe_price_id: "price_1IjUhHBsEkZw4Q05Kal22OJj", amount: 1000_00, interval: "year")
+Plan.create(name: "Small", stripe_id: "small-monthly", stripe_price_id: "price_1IjUjlBsEkZw4Q05BaQsnkY4", amount: 25_00, interval: "month")
+Plan.create(name: "Small", stripe_id: "small-annual", stripe_price_id: "price_1IjUjUBsEkZw4Q05RXEzOI1s", amount: 75_00, interval: "year")
+Plan.create(name: "Large", stripe_id: "large-monthly", stripe_price_id: "price_1IjUi4BsEkZw4Q05a7b6Q0Kc", amount: 100_00, interval: "month")
+Plan.create(name: "Large", stripe_id: "large-annual", stripe_price_id: "price_1IjUhHBsEkZw4Q05Kal22OJj", amount: 1000_00, interval: "year")
