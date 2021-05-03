11085fcf32354a4a8da3b4bd22b4a4bd52f7bb82
Marji Symonds <marji712@gmail.com>
2021-05-03 08:40:34 -0700

    Add subscriptions controller

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,14 @@
+class SubscriptionsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_plan, only: [:new, :create, :update]
+
+    def new
+        byebug
+    end
+
+    private
+        def set_plan
+            @plan = Plan.find_by(id: params[:plan_id])
+            redirect_to pricing_path if @plan.nil?
+        end
+end
