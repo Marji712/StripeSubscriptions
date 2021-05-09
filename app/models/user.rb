87750c1560aa95d58571bfdacefd9f85611a5a57
Marji Symonds <marji712@gmail.com>
2021-05-09 11:22:22 -0700

    Add PaymentsController and add route for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_person_name
 
   has_many :notifications, foreign_key: :recipient_id
-  # has_many :notifications, as: :recipient
   has_many :services
   has_many :subscriptions
   has_many :charges
