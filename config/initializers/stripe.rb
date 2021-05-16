220c78e16c0be68f419c27fd84cf9951410ab86b
Marji Symonds <marji712@gmail.com>
2021-05-16 16:05:03 -0700

    Add through project 7

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,14 @@
-Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+Stripe.api_key= Rails.application.credentials.stripe[:private_key]
 
 class PaymentIncomplete < StandardError
-  attr_reader :payment_intent
+	attr_reader :payment_intent
 
-  def initialize(payment_intent)
-    @payment_intent = payment_intent
-  end
+	def initialize(payment_intent)
+		@payment_intent = payment_intent
+	end
+end
+
+StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET'] || Rails.application.credentials.stripe[:signing_secret]
+StripeEvent.configure do |events|
+	events.subscribe 'customer.subscription.updated', SubscriptionUpdatedWebhook.new
 end
