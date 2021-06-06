ade6e7b48b285d422aeafa9dea0c953ed73ab321
Marji Symonds <marji712@gmail.com>
2021-06-06 13:34:40 -0700

    Add ability to cancel and resume subscription - 3.11

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -28,6 +28,15 @@ class SubscriptionsController < ApplicationController
         redirect_to subscription_path, notice: "You have successfully changed plans"
     end
 
+    def destroy
+        current_user.subscription.cancel
+        redirect_to subscription_path, notice: "Your subscription is canceled."
+    end
+
+    def resume
+        current_user.subscription.resume
+        redirect_to subscription_path, notice: "Your subscription has been resumed."
+    end
     private
 
         def set_plan
